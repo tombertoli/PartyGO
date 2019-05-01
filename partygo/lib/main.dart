d10d08a557fb2c1a6d5b6fa4f580b996719f3456
Tomás Bertoli <tomasberto85@gmail.com>
2019-04-30 22:37:47 -0300

    Empezada la interfaz de creacion de fiestas.

--- partygo/lib/main.dart
+++ partygo/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,21 +7,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return CupertinoApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Main Page'),
     );
   }
 }
@@ -57,55 +46,65 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  List<Widget> _divide(List<Widget> l) {
+    for (var i = 1; i < l.length; i += 2) {
+      l.insert(i, Divider());
+
+      if (l.length - i <= 2) break;
+    }
+
+    return l;
+  }
+
+  List<T> _concat<T>(List<T> list, {List<T> addingList, T addingItem}) {
+    if (addingList != null) list.addAll(addingList);
+    if (addingItem != null) list.add(addingItem);
+    
+    return list;
+  }
+
+  void _dateChanged(DateTime dt) {
+    return;
+  }
+
+  void _privacyChanged(Privacy p) {
+    return;
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have tapped the button this many times:',
+    return CupertinoPageScaffold(      
+      child: CupertinoScrollbar(
+        child: CustomScrollView(
+          slivers: [
+            CupertinoSliverNavigationBar(
+              automaticallyImplyLeading: false,
+              largeTitle: Text('Nueva Fiesta'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+            SliverList(
+              delegate: SliverChildListDelegate(
+                _divide([
+                  CupertinoTextField(),
+                  CupertinoDatePicker(onDateTimeChanged: _dateChanged,),
+                  CupertinoSegmentedControl(
+                    children: {
+                      Privacy.public: Text('Public'),
+                      Privacy.inviteOnly: Text('Invite-Only'),
+                      Privacy.friends: Text('Friends Only'),
+                      Privacy.friendsOfFriends: Text('Friends of Friends'),
+                    },
+                    onValueChanged: _privacyChanged,
+                  )
+                ])
+              )
+            )
+          ]
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
-  }
+  }    
+}
+
+enum Privacy {
+  public, inviteOnly, friends, friendsOfFriends
 }
